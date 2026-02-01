89632870642be712a1a029f9666288cd76b2abc0
Fabian Rata <fabianrata@Fabians-Mac-mini.local>
2026-02-01 21:27:26 +0000

    solving img error

--- src/components/Landing_navbar/Landing_navbar.jsx
+++ src/components/Landing_navbar/Landing_navbar.jsx
@@ -5,7 +5,7 @@ const Landing_navbar = () => {
   return (
     <div className={styles.navbar_container}>
       <div className={styles.navbar_logo_container}>
-         <Image src="/landing_logo.jpg" alt="logo" width={120} height={60} />
+         <Image src="./landing_logo.jpg" alt="logo" width={120} height={60} />
       </div>
     </div>
   )
